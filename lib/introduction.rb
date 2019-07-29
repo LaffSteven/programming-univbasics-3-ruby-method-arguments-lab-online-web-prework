4060f55141c0a6a66166749ba399c85f352a7349
Steven Laff <steve.laff@gmail.com>
2019-07-29 18:39:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."  
 end
 
+#Had to go look this up on the internet
+#How to create an optional argument was not covered in the previous section
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
